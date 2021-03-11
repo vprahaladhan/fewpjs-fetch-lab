f7535beff08a18fb5382b3b71849ccdd6ef843e5
prahal <vprahaladhan@gmail.com>
2021-03-11 19:41:32 +1100

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  fetch('https://anapioficeandfire.com/api/books')
+  return fetch('https://anapioficeandfire.com/api/books')
     .then(res => res.json())
     .then(books => renderBooks(books));
 }
